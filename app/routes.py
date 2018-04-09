e79b651bf639b0136fed6a5cfec071525fdcf940
TrueTony <spanchbob@gmail.com>
2018-04-09 13:56:47 +0300

    lesson 2 add templates/index.html and base.html change return in routers

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет мир!"
+    user = {'username': 'Anton'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie wa cool!'
+        },
+        {
+            'author': {'username': 'Иполит'},
+            'body': 'Is`s so disgusting, your fish in oil!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
