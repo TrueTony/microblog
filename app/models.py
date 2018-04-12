55e93bc588b07547b3e339c80cc434f776e52e48
spanchbob@gmail.com <Lkzgbnjyf777>
2018-04-12 16:24:44 +0300

    add class Post

--- app/models.py
+++ app/models.py
@@ -1,11 +1,23 @@
+from datetime import datetime
 from app import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary=_ket=Trye)
-    username = db.Column(db.String(64), index.=True, unique=True)
-    email = db.Column(db.String(120), undex=True, unique=true)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), undex=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backef='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primaty_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
