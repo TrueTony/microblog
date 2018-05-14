8b084f19208bf60dcb737ffb12732a84d35c011a
spanchbob@gmail.com <Lkzgbnjyf777>
2018-05-14 17:58:38 +0300

    add follower and followed

--- app/models.py
+++ app/models.py
@@ -6,6 +6,11 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
+followers = db.Table('followers',
+                    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                    )
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -19,6 +24,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    follower = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
