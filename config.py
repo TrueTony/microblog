cae27adf06d70593a40569815e148bb777068785
spanchbob@gmail.com <Lkzgbnjyf777>
2018-05-21 17:57:08 +0300

    add reset password

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['sfp10g@yandex.ru']
     POSTS_PER_PAGE = 3
