296b2011901cfae2cc6b1f2ecdb957825724d30b
TrueTony <spanchbob@gmail.com>
2018-05-22 17:58:25 +0300

    translate en->ru

--- app/forms.py
+++ app/forms.py
@@ -12,7 +12,7 @@ class LoginForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
     password = PasswordField(_l('Password'), validators=[DataRequired()])
     remember_me = BooleanField(_l('Remember Me'))
-    submit = SubmitField(_l('Sing in'))
+    submit = SubmitField(_l('Sing In'))
 
 
 class RegistrationForm(FlaskForm):
