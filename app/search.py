fb8f51183573f08cb80564345cbfd46be33519ae
TrueTony <spanchbob@gmail.com>
2018-05-30 17:03:35 +0300

    fixs

--- app/search.py
+++ app/search.py
@@ -19,7 +19,7 @@ def query_index(index, query, page, per_page):
     if not current_app.elasticsearch:
         return [], 0
     search = current_app.elasticsearch.search(
-        index=index, dot_type=index,
+        index=index, doc_type=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
               'from': (page - 1) * per_page, 'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
