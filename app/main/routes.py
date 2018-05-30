2d208178c0bb51c96102908f26a695d443b7755c
TrueTony <spanchbob@gmail.com>
2018-05-30 19:48:27 +0300

    upd

--- app/main/routes.py
+++ app/main/routes.py
@@ -57,8 +57,9 @@ def explore():
         if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title=_('Explore'), posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
+    return render_template('index.html', title=_('Explore'),
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/user/<username>')
@@ -68,12 +69,12 @@ def user(username):
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.user', username=user.username, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) \
-        if posts.has_prev else None
+    next_url = url_for('main.user', username=user.username,
+                       page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.user', username=user.username,
+                       page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_num=prev_url)
+                           next_url=next_url, prev_url=prev_url)
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
@@ -84,49 +85,54 @@ def edit_profile():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash(_('Your changes have been saved'))
+        flash(_('Your changes have been saved.'))
         return redirect(url_for('main.edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title=_('Edit Profile'), form=form)
+    return render_template('edit_profile.html', title=_('Edit Profile'),
+                           form=form)
+
 
 @bp.route('/follow/<username>')
 @login_required
 def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash(_('User %(username)s not found', username=username))
+        flash(_('User %(username)s not found.', username=username))
         return redirect(url_for('main.index'))
     if user == current_user:
-        flash(_('You cannot follow yourself'))
+        flash(_('You cannot follow yourself!'))
         return redirect(url_for('main.user', username=username))
     current_user.follow(user)
     db.session.commit()
     flash(_('You are following %(username)s!', username=username))
     return redirect(url_for('main.user', username=username))
 
+
 @bp.route('/unfollow/<username>')
 @login_required
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash(_('User %(username)s not found', username=username))
+        flash(_('User %(username)s not found.', username=username))
         return redirect(url_for('main.index'))
     if user == current_user:
-        flash(_('You cannot unfollow yourself'))
-        redirect(url_for('main.user', username=username))
+        flash(_('You cannot unfollow yourself!'))
+        return redirect(url_for('main.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
-    flash(_('You are unfollow %(username)s', username=username))
+    flash(_('You are not following %(username)s.', username=username))
     return redirect(url_for('main.user', username=username))
+
 
 @bp.route('/translate', methods=['POST'])
 @login_required
 def translate_text():
     return jsonify({'text': translate(request.form['text'],
-    request.form['source_language'],
-    request.form['dest_language'])})
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
+
 
 @bp.route('/search')
 @login_required
@@ -142,7 +148,3 @@ def search():
         if page > 1 else None
     return render_template('search.html', title=_('Search'), posts=posts,
                            next_url=next_url, prev_url=prev_url)
-
-
-
-
