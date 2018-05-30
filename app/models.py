ebdf7f864af496dd6e5d62de98d5a66540d33663
TrueTony <spanchbob@gmail.com>
2018-05-30 19:43:25 +0300

    upd

--- app/models.py
+++ app/models.py
@@ -1,18 +1,13 @@
-from app import login
-from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db
-from flask_login import UserMixin
 from hashlib import md5
 from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
-from app import current_app
+from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
 
-followers = db.Table('followers',
-                    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-                    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-                    )
 
 class SearchableMixin(object):
     @classmethod
@@ -29,25 +24,40 @@ class SearchableMixin(object):
     @classmethod
     def before_commit(cls, session):
         session._changes = {
-            'add': [obj for obj in session.new if isinstance(obj, cls)],
-            'update': [obj for obj in session.dirty if isinstance(obj, cls)],
-            'delete': [obj for obj in session.deleted if isinstance(obj, cls)]
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
         }
 
     @classmethod
     def after_commit(cls, session):
         for obj in session._changes['add']:
-            add_to_index(cls.__tablename__, obj)
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
         for obj in session._changes['update']:
-            add_to_index(cls.__tablename__, obj)
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
         for obj in session._changes['delete']:
-            remove_from_index(cls.__tablename__, obj)
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
         session._changes = None
 
     @classmethod
     def reindex(cls):
         for obj in cls.query:
             add_to_index(cls.__tablename__, obj)
+
+
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+
+
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -61,8 +71,7 @@ class User(UserMixin, db.Model):
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
-    )
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -75,37 +84,39 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
-        if self.is_following:
+        if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
-        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-            followers.c.follower_id == self.id)
+                followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
-
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
             id = jwt.decode(token, current_app.config['SECRET_KEY'],
-                            algorithm=['HS256'])['reset_password']
+                            algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
@@ -115,16 +126,14 @@ class User(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
+
 class Post(SearchableMixin, db.Model):
+    __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     language = db.Column(db.String(5))
-    __searchable__ = ['body']
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-db.event.listen(db.session, 'before_commit', Post.before_commit)
-db.event.listen(db.session, 'after_commit', Post.after_commit)
